654c35ced616e5c1a3cbb3ffb9a2aaca74a53851
Alessandro9827 <ale.moramarco27@gmail.com>
2023-11-21 19:54:33 +0100

    generete random number

--- script.js
+++ script.js
@@ -7,41 +7,40 @@
 //Creo la sezione main e il button play
 const mainContentEl = document.querySelector('main section.main-content');
 const playButton = document.querySelector('button#btn');
+let numberArrey = createArreyOfNumber (1, 100);
 
-playButton.addEventListener('click', function(){
+playButton.addEventListener('click', function() {
     for (let i = 1 ; i <= 100 ; i++){
         //creo un nuovo elemento quadrato, una nuova cella nel mio quadrato
         const currentSquare = getNewSquare();
     
-        //inizializzo il suo contenuto per poterlo utilizzare anche piu' avanti
-        const squareContent = i;
-    
-        //aggiungo il contenuto all'elemento che voglio popolare
-        currentSquare.innerHTML += `<span> ${squareContent} </span>`;
-    
-        //quando clicco su una di queste celle
+        //aggiungo il numero alla casella
+        let randomIndex = getRandomNumber (0, numberArrey.length -1);
+        let randomNumber = numberArrey [randomIndex];
+        numberArrey.slice(randomIndex, 1);
+        currentSquare.innerHTML += `<span> ${randomIndex} </span>`;
+        
+        //quando clicco su una cella si colora di azzurro
         currentSquare.addEventListener('click', function(){
-            // % metto o tolgo la classe css clicked allo stesso elemento
-            currentSquare.classList.toggle('clicked');
-            currentSquare.classList.add('bg-blue');
-            console.log('hai selezionato la casella n.', );
+        currentSquare.classList.toggle('clicked');
+        currentSquare.classList.add('bg-blue');
+        console.log('hai selezionato la casella n.', randomIndex);
         });
-    
-        // & aggiungo la cella completa all'elemento a cui voglio aggiungerla nel DOM.
+        
+        //aggiungo la cella con il numero nel DOM.
         mainContentEl.appendChild(currentSquare);
     }
 });
 
 
-
-// ? ------ Functions ------ ?
+//------ Functions ------
 
 /**
  * Creates a new square element, an article with class 'item-square' and returns it to the caller.
  *
  * @returns The new created square element.
  */ 
-function getNewSquare(){
+function getNewSquare() {
     const newSquareElement = document.createElement('article');
     newSquareElement.classList.add('item-square');
     return newSquareElement;
@@ -54,6 +53,15 @@ function getNewSquare(){
  * @param maxNumber The maximum required number, included
  * @returns The new randomly generated number
  */
-function getRandomNumber(minNumber, maxNumber){
+function getRandomNumber(minNumber, maxNumber) {
     return Math.floor( Math.random() *(maxNumber - minNumber + 1) + minNumber);
 }
+
+function createArreyOfNumber (start, end) {
+    let myArrey = [];
+    for (i = start ; i <= end ; i++) {
+        myArrey.push(i);
+    }
+    return myArrey;
+}
+
